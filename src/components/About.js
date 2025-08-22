58a5dfa10699d469b8081ac5aee8cda84b951c3d
MikelMrad <mikelmrad.work@gmail.com>
2025-08-22 16:42:45 +0300

    Fixed Typo

--- src/components/About.js
+++ src/components/About.js
@@ -10,7 +10,7 @@ export default function About() {
       <AboutContainer src={USEK} header={"Computer Science Student"} paragraph={"I graduated from the Holy Spirit University of Kaslik with a Bachelor's degree in Computer Science."} date={"September 2021 - June 2025"}/>
       <AboutContainer src={EST} header={"Web Development Intern"} paragraph={"Started as a Web Development Intern at EST to fulfill the university's internship requirement."} date={"July 2024 - October 2024"}/>
       <AboutContainer src={EST} header={"Part-Time Front-End Developer"} paragraph={"After completing my internship, I continued working part-time until I received my diploma."} date={"November 2024 - June 2025"}/>
-      <AboutContainer src={EST} header={"Full-Time Front-End Developer"} paragraph={"With my diploma in hand, I secured a full-time position as a Front-End Developer."} date={"June 2025 - Present"}/>
+      <AboutContainer src={EST} header={"Front-End Developer"} paragraph={"With my diploma in hand, I secured a full-time position as a Front-End Developer."} date={"June 2025 - Present"}/>
     </section>
   )
 }
