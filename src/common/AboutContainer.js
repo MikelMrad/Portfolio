015de37f13d361ef0fd761c721df5885ea040155
Mikel Mrad <mikelmrad.work@gmail.com>
2026-04-08 12:50:38 +0300

    about container

--- src/common/AboutContainer.js
+++ src/common/AboutContainer.js
@@ -1,39 +1,21 @@
-import styles from "@/styles/home.module.css"
 import Image from "next/image"
+import styles from "@/styles/about.module.css"
 
-export default function AboutContainer({src , header , paragraph , date}) {
+export default function AboutContainer({src, header, paragraph, date}) {
   return (
-      <div 
-      style={{
-        display:"flex",
-        alignItems:"center",
-        gap:"10px",
-        width:"100%",
-        maxWidth:"800px"
-      }}>
-        <Image 
+    <div className={styles.container}>
+      <Image
         src={src}
         height={60}
         width={60}
-        style={{borderRadius:"50%"}}
-        alt="Image"/>
-        <div
-        style={{
-          display:"flex",
-          flexDirection:"column",
-          alignItems:"flex-start",
-          gap:"0",
-        }}>
-          <h1 style={{fontSize:"22px" , margin: 0}}>
-            {header}
-          </h1>
-          <p style={{fontSize:"16px",textAlign:"left" , margin: 0}}>
-            {paragraph}
-          </p>
-          <h6 style={{fontSize:"14px",color:"#909090" , margin: 0}}>
-            {date}
-          </h6>
-        </div>
+        className={styles.image}
+        alt="Image"
+      />
+      <div className={styles.body}>
+        <h1 className={styles.header}>{header}</h1>
+        <p className={styles.paragraph}>{paragraph}</p>
+        <h6 className={styles.date}>{date}</h6>
       </div>
+    </div>
   )
 }
